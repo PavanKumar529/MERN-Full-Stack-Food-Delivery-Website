7a2ffa4f756d9009e27ae36469c4bcad9f058de3
Pavan Kumar Kuruva <kpavankumark9783@gmail.com>
2024-06-14 18:54:15 +0530

    Third Commit: Display  category wise items

--- client/src/components/FoodDisplay/FoodDisplay.jsx
+++ client/src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,7 +3,7 @@ import "./FoodDisplay.css"
 import { StoreContext } from '../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
 
-const FoodDisplay = (category) => {
+const FoodDisplay = ({category}) => {
     const { food_list } = useContext(StoreContext)
 
   return (
@@ -12,7 +12,9 @@ const FoodDisplay = (category) => {
         <div className="food-display-list">
             {
                 food_list.map((item, index) => {
-                    return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+                    if(category === "All" || category === item.category) {
+                        return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+                    }
                 })
             }
         </div>
