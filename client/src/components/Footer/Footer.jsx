bfe650d63bdd629d3ac5a0652bf5ff85800a7e05
Pavan Kumar Kuruva <kpavankumark9783@gmail.com>
2024-06-14 23:43:00 +0530

    Fifth Commit: Created most Responsive App at different screen sizes like 1050px,900px,750px,500px, 350px

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,41 +1,41 @@
-import React from 'react'
-import "./Footer.css"
-import { assets } from '../../assets/assets'
+import React from 'react';
+import "./Footer.css";
+import { assets } from '../../assets/assets';
 
 const Footer = () => {
   return (
     <div className='footer'>
-        <div className="footer-content">
-            <div className="footer-content-left">
-                <img src={assets.logo} alt="" />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque quam dolorem sapiente, sequi atque ad id consequuntur eveniet illum enim nobis quae voluptates inventore alias veritatis? Non saepe vel vitae.</p>
-                <div className="footer-social-media-icons">
-                    <img src={assets.facebook_icon} alt="" />
-                    <img src={assets.twitter_icon} alt="" />
-                    <img src={assets.linkedin_icon} alt="" />
-                </div>
-            </div>
-            <div className="footer-content-center">
-                <h2>Company</h2>
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Delivery</li>
-                    <li>Privacy Policy</li>
-                </ul>
-            </div>
-            <div className="footer-content-left">
-                <h2>GET IN TOUCH</h2>
-                <ul>
-                    <li>+91 8328533863</li>
-                    <li>pavankumarkuruva529@gmail.com</li>
-                </ul>
-            </div>           
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="logo" />
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque quam dolorem sapiente, sequi atque ad id consequuntur eveniet illum enim nobis quae voluptates inventore alias veritatis? Non saepe vel vitae.</p>
+          <div className="footer-social-media-icons">
+            <img src={assets.facebook_icon} alt="Facebook" />
+            <img src={assets.twitter_icon} alt="Twitter" />
+            <img src={assets.linkedin_icon} alt="LinkedIn" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>Company</h2>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>Get in Touch</h2>
+          <ul>
+            <li>+91 8328533863</li>
+            <li>pavankumarkuruva529@gmail.com</li>
+          </ul>
+        </div>
       </div>
       <hr />
-      <p className='footer-copyright'>Copyright 2024  @ Tomato.com - All Right Reserved</p> 
+      <p className='footer-copyright'>Copyright 2024 © Tomato.com - All Rights Reserved</p>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,31 +1,35 @@
-// import React from 'react'
-import "./Navbar.css"
-import { assets } from "../../assets/assets"
-import { useState } from "react"
-
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+import { useState } from "react";
+import { Link } from 'react-router-dom'
 const Navbar = () => {
+    const [menu, setMenu] = useState("menu");
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const [ menu, setMenu ] = useState("menu")
-
-  return (
-    <div className="navbar">
-        <img src={assets.logo} alt="logo" className="logo"/>
-        <ul className="navbar-menu">
-            <li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</li>
-            <li onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</li>
-            <li onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>Mobile App</li>
-            <li onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>Contact Us</li>
-        </ul>
-        <div className="navbar-right">
-            <img src={assets.search_icon} alt="" />
-            <div className="navbar-search-icon">
-                <img src={assets.basket_icon} alt="basket_icon" />
-                <div className="dot"></div>
+    return (
+        <div className="navbar">
+            <img src={assets.logo} alt="logo" className="logo" />
+            <ul className={`navbar-menu ${isMenuOpen ? "open" : ""}`}>
+                <Link to='/'><li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</li></Link>
+                <a href="#explore-menu"><li onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</li></a>
+                <a href="#app-download"><li onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>Mobile App</li></a>
+                <a href="#footer"><li onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>Contact Us</li></a>
+            </ul>
+            <div className="navbar-right">
+                <img src={assets.search_icon} alt="search icon" />
+                <div className="navbar-search-icon">
+                    <img src={assets.basket_icon} alt="basket icon" />
+                    <div className="dot"></div>
+                </div>
+                <button>sign in</button>
             </div>
-            <button>sign in</button>
-        </div>      
-    </div>
-  )
-}
+            <div className="hamburger" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    );
+};
 
-export default Navbar
+export default Navbar;
